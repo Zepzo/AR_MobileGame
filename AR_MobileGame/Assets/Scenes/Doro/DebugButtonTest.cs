8e4b59c6fc49fb6ad9d911f141b984cd4eeb8b5b
DorotheaWunder <dorothea.wunder@student.forsbergsskola.se>
2025-07-29 14:33:05 +0200

    Set up Menu scene with canvas

---
+++ AR_MobileGame/Assets/Scenes/Doro/DebugButtonTest.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class DebugButtonTest : MonoBehaviour
+{
+    public GameObject button;
+    void Update()
+    {
+        DebugDrag();
+    }
+    
+    public void PrintDebug()
+    {
+        Debug.Log("You just pressed the button: " + gameObject.name);
+
+        button = gameObject;
+        Debug.Log("You just pressed the button: " + button.name);
+    }
+
+    public void DebugDrag()
+    {
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+
+            if (touch.phase == TouchPhase.Began)
+            {
+                Debug.Log("Touch started at: " + touch.position);
+            }
+            else if (touch.phase == TouchPhase.Ended)
+            {
+                Debug.Log("Touch has ended at: " + touch.position);
+            }
+        }
+    }
+
+    public void TapCount()
+    {
+        
+    }
+}
