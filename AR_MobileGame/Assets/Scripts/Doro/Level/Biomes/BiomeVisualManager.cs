39a00d4fa15b3ef564919a0bd3817de741490316
DorotheaWunder <dorothea.wunder@student.forsbergsskola.se>
2025-08-07 14:30:30 +0200

    expanded biomes to foreground

--- AR_MobileGame/Assets/Scripts/Doro/Level/Biomes/BiomeVisualManager.cs
+++ AR_MobileGame/Assets/Scripts/Doro/Level/Biomes/BiomeVisualManager.cs
@@ -25,7 +25,13 @@ public class BiomeVisualManager : MonoBehaviour
         ParallaxManager.Layers[3].SetSprite(biome.VegetationSpriteA);
         ParallaxManager.Layers[4].SetSprite(biome.VegetationSpriteB);
         
-        //ParallaxManager.Layers[5].SetSprite(biome.GroundSprite);
+        ParallaxManager.Layers[5].SetSprite(biome.GroundSprite);
+        
+        ParallaxManager.Layers[6].SetSprite(biome.FrontSpriteA);
+        ParallaxManager.Layers[7].SetSprite(biome.FrontSpriteB);
+        ParallaxManager.Layers[8].SetSprite(biome.FrontSpriteC);
+        ParallaxManager.Layers[9].SetSprite(biome.FrontSpriteD);
+        ParallaxManager.Layers[10].SetSprite(biome.FrontSpriteE);
     }
     
     public void ApplyBiomeColorsBG(SO_BiomePalette biome)
@@ -38,7 +44,13 @@ public class BiomeVisualManager : MonoBehaviour
         ParallaxManager.Layers[3].SetColor(biome.VegetationAColor);
         ParallaxManager.Layers[4].SetColor(biome.VegetationBColor);
         
-        //ParallaxManager.Layers[5].SetColor(biome.GroundColor);
+        ParallaxManager.Layers[5].SetColor(biome.GroundColor);
+        
+        ParallaxManager.Layers[6].SetColor(biome.FrontColorA);
+        ParallaxManager.Layers[7].SetColor(biome.FrontColorB);
+        ParallaxManager.Layers[8].SetColor(biome.FrontColorC);
+        ParallaxManager.Layers[9].SetColor(biome.FrontColorD);
+        ParallaxManager.Layers[5].SetColor(biome.FrontColorE);
     }
     
     public void TransitionBiomeColorsBG(SO_BiomePalette biome, float transitionTime)
@@ -52,7 +64,13 @@ public class BiomeVisualManager : MonoBehaviour
         ParallaxManager.Layers[3].SetTargetColor(biome.VegetationAColor, transitionTime);
         ParallaxManager.Layers[4].SetTargetColor(biome.VegetationBColor, transitionTime);
         
-        //ParallaxManager.Layers[5].SetTargetColor(biome.GroundColor, transitionTime);
+        ParallaxManager.Layers[5].SetColor(biome.GroundColor);
+        
+        ParallaxManager.Layers[6].SetColor(biome.FrontColorA);
+        ParallaxManager.Layers[7].SetColor(biome.FrontColorB);
+        ParallaxManager.Layers[8].SetColor(biome.FrontColorC);
+        ParallaxManager.Layers[9].SetColor(biome.FrontColorD);
+        ParallaxManager.Layers[5].SetColor(biome.FrontColorE);
     }
 }
 //other methods that spawn the vegetation props
